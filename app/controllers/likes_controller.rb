db83ddf89cb3688117c93a1f2b5e45600bf9d78d
Raul Palacios <palacios.raul02@gmail.com>
2021-04-11 00:37:44 -0500

    Add increment likes and retweets

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,8 @@
+class LikesController < ApplicationController
+  def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @tweet.increment! :likes
+
+    redirect_to @tweet
+  end
+end
---
+++ app/controllers/retweets_controller.rb
@@ -0,0 +1,8 @@
+class RetweetsController < ApplicationController
+  def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @tweet.increment! :retweets
+
+    redirect_to @tweet
+  end
+end
